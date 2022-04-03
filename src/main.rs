850ca69459a282988d18e89dd69d2bb615f9f583
Zachary Petro <zmpetro@gmail.com>
2022-04-03 16:11:37 -0400

    pull groupme msg data from POST json

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,24 @@ use actix_web::{web, App, HttpServer, Result};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct Info {
-    username: String,
+struct Msg {
+    name: String,
+    text: String,
+    user_id: String,
 }
 
-async fn index(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
+async fn index(msg: web::Json<Msg>) -> Result<String> {
+    print!("\nname: {}\ntext: {}\nuser_id: {}\n",
+                msg.name,
+                msg.text,
+                msg.user_id);
+    Ok(format!("Welcome {}!", msg.name))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/", web::post().to(index)))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 9300))?
         .run()
         .await
 }
