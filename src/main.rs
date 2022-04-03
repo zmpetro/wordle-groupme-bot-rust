e3ced592239cb3c5ed2748b633a9135abdb5ccf1
Zachary Petro <zmpetro@gmail.com>
2022-04-03 17:17:56 -0400

    detect wordle scores and commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, App, HttpServer, Result};
+#[macro_use]
+extern crate lazy_static;
+
+use actix_web::{web, App, HttpServer, HttpResponse};
+use regex::Regex;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -8,17 +12,32 @@ struct Msg {
     user_id: String,
 }
 
-async fn index(msg: web::Json<Msg>) -> Result<String> {
-    print!("\nname: {}\ntext: {}\nuser_id: {}\n",
+lazy_static! {
+    static ref WORDLE_SCORE: Regex = {
+        Regex::new(r"^Wordle\s\d+\s[1-6X]/6").unwrap()
+    };
+    static ref WORDLE_CMD: Regex = {
+        Regex::new(r"^/wordle").unwrap()
+    };
+}
+
+async fn wordle(msg: web::Json<Msg>) -> HttpResponse {
+    if WORDLE_SCORE.is_match(&msg.text) {
+        println!("WORDLE SCORE DETECTED")
+    } else if WORDLE_CMD.is_match(&msg.text) {
+        println!("WORDLE COMMAND DETECTED")
+    }
+    // Change this to logging
+    print!("name: {}\ntext: {}\nuser_id: {}\n\n",
                 msg.name,
                 msg.text,
                 msg.user_id);
-    Ok(format!("Welcome {}!", msg.name))
+    HttpResponse::Ok().finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::post().to(index)))
+    HttpServer::new(|| App::new().route("/", web::post().to(wordle)))
         .bind(("0.0.0.0", 9300))?
         .run()
         .await
